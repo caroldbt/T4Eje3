cda14511a2f6ddac9824b07f0882f8d717e4fa1b
caroldbt <carol123dbt@gmail.com>
2021-07-09 22:10:39 +0200

    T4Eje3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T4Eje3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
